ffce4a8d02c0f89a19933d017722ad837f8d16f4
Sumesh <sumeshstha94@gmail.com>
2023-06-02 15:41:30 +0545

    Lab4

--- include/AbstractBST.h
+++ include/AbstractBST.h
@@ -9,5 +9,10 @@ class AbstractBST {
     virtual void min(int &output)=0;
     virtual bool exists(int targetKey)=0;
     virtual void inorder()=0;
+    //For link Data Structure:
+    virtual bool isEmpty() = 0;
+    virtual void addBST(int key, int value) = 0;
+    virtual void removeBST(int keyToDelete) = 0;
+    virtual bool searchBST(int targetKey) = 0;
 };
 #endif
